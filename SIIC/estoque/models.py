4cde91b56ce32a4a4f8263e375b01f12751f5b76
admin <admin@admins-MacBook-Pro.local>
2021-05-16 23:54:28 -0300

    Atualizações diversas

--- SIIC/estoque/models.py
+++ SIIC/estoque/models.py
@@ -1,5 +1,6 @@
 from django.db.models.fields import FloatField
 from django.db.models.fields.files import FieldFile, FileField
+from django.db.models.fields.related import ForeignKey
 from usuarios.models import Usuario
 from django.db import models
 from django.contrib.auth.models import User
