a9f3a7df5ab86f9d8a1581f7930954043e9857d7
Fabiano Sato <61922679+devfsato@users.noreply.github.com>
2021-05-26 20:54:26 -0300

    merge

--- SIIC/relatorio/forms.py
+++ SIIC/relatorio/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from estoque.models import Estoque
+from relatorio.models import TOTAL_PRODUTOS, TOTALIZADOR
 
 #class NameForm(forms.Form):
 #    name = forms.CharField(label='Name', max_length=100, help_text='inserir nome', initial='Name')
 
-class Relatorio(forms.ModelForm):
-    class Meta:
-        model = Estoque
-        fields = '__all__'
+# class Relatorio(forms.ModelForm):
+#     class Meta:
+#         model = TOTAL_PRODUTOS
+#         fields = '__all__'
+
+# class Relatorio_Total(forms.ModelForm):
+#     class Meta:
+#         model = TOTALIZADOR
+#         fields = '__all__'
--- SIIC/relatorio/models.py
+++ SIIC/relatorio/models.py
@@ -1,16 +1,48 @@
 from django.db import models
 
+class TOTAL_PRODUTOS(models.Model):
+    ID = models.PositiveIntegerField()
+    CREATED = models.DateTimeField()
+    NOME_PRODUTO = models.CharField(max_length=50)
+    QUANTIDADE = models.PositiveIntegerField()
+    MOVIMENTO = models.CharField(max_length=50)
+    TOTAL = models.DecimalField(max_digits=10, decimal_places=2)
 
-class Report(models.Model):
-
-    title = models.CharField('Título', max_length=100)
-    date = models.DateField('Data', auto_now_add=True)
-    description = models.TextField('Descrição')
-
-    def __str__(self):
-        return self.title
-    
     class Meta:
-        verbose_name = 'Relatório'
-        verbose_name_plural = 'Relatórios'
-        ordering = ['-id']
+        managed = False
+        db_table = 'TOTAL_PRODUTOS'
+
+class TOTALIZADOR(models.Model):
+    ID = models.PositiveIntegerField()
+    MOVIMENTO = models.CharField(max_length=50)
+    QUANTIDADE = models.PositiveIntegerField()
+    TOTAL = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'TOTALIZADOR'
+    
+
+
+class RESUMO_FINANC(models.Model):
+    VALOR = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'RESUMO_FINANC'
+
+# class Report(models.Model):
+
+#     title = models.CharField('Título', max_length=100)
+#     date = models.DateField('Data', auto_now_add=True)
+#     description = models.TextField('Descrição')
+
+#     def __str__(self):
+#         return self.title
+    
+#     class Meta:
+#         verbose_name = 'Relatório'
+#         verbose_name_plural = 'Relatórios'
+#         ordering = ['-id']
+
+
